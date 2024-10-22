2ba6b4139f0da24014b190b0a74d7bfba2679c3c
freyds <fradesaldrin18@gmail.com>
2024-10-23 04:33:34 +0800

    App.js

--- FRONT/frontend/src/App.js
+++ FRONT/frontend/src/App.js
@@ -5,12 +5,13 @@ import AddProduct from './components/AddProduct';
 import EditProduct from './components/EditProduct';
 import DeleteProduct from './components/DeleteProduct';
 import ViewProduct from './components/ViewProduct';
-
+import Dashboard from './components/Dashboard'; 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} /> 
         <Route path="/add-product" element={<AddProduct />} />
         <Route path="/edit-product/:id" element={<EditProduct />} />
         <Route path="/delete-product/:id" element={<DeleteProduct />} />
